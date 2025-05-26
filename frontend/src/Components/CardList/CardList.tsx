80116eecdcb2d96866e6636640d46b54434220c5
Rorisang19 <rorisangputu@gmail.com>
2025-05-26 14:56:40 +0200

    refactor: simplify CardList component to dynamically render Card components from companies array

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
 import Card from '../Card/Card'
 
-interface Props = {
-    
-}
 
-const CardList = (props: Props) => {
+const CardList = () => {
   return (
-    <div>
-        <Card/>
-        <Card/>
-        <Card/>
+    <div className='flex flex-col gap-2'>
+        {companies.map((co) => (
+            <Card 
+                companyName={co.name} 
+                ticker={co.ticker} 
+                price={co.price}
+            />
+        ))}
     </div>
   )
 }
 
 export default CardList
+
+const companies = [
+    {
+        name: "APPLE",
+        ticker: "AAPL",
+        price: 150
+    },
+    {
+        name: "MICROSOFT",
+        ticker: "MSFT",
+        price: 280
+    },
+    {
+        name: "TESLA",
+        ticker: "TSLA",
+        price: 700
+    }
+]
