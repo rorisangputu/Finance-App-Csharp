4760c5af9656f71b9b1953b89f3bb5785377dcab
Rorisang19 <rorisangputu@gmail.com>
2025-05-27 15:10:07 +0200

    fix: update Search component to use onSearchSubmit and handleSearchChange props for improved functionality

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,24 +1,18 @@
 import {  type ChangeEvent, type SyntheticEvent } from 'react'
 
 interface searchProps {
-    onClick: (e: SyntheticEvent) => void,
-    handleChange:(e: ChangeEvent<HTMLInputElement>) => void,
+    onSearchSubmit: (e: SyntheticEvent) => void,
+    handleSearchChange:(e: ChangeEvent<HTMLInputElement>) => void,
     search: string
 }
-const Search = ({onClick, handleChange, search}:searchProps) => {
+const Search = ({onSearchSubmit, handleSearchChange, search}:searchProps) => {
     
   return (
     <div className='border py-5'>
-        <h1>Search:</h1>
-        <form action="">
-            <input 
-                type="text" 
-                value={search} 
-                onChange={(e) => handleChange(e)} 
-                className='border rounded-md p-2'
-            />
-            <button onClick={(e) => onClick(e)} className='bg-blue-700 text-white p-2'>Submit</button>
-        </form>
+      <h1>Search:</h1>
+      <form onSubmit={onSearchSubmit}>
+          <input value={search} onChange={handleSearchChange} />
+      </form>
     </div>
   )
 };
