12f6055eefc519dd69cd3f57d376e98c2ed87550
Rorisang19 <rorisangputu@gmail.com>
2025-05-27 08:43:27 +0200

    feat: implement search input functionality in Search component

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-type Props = {}
+type Props = {
+
+}
 
 const Search = (props: Props) => {
+    const [search, setSearch] = useState<string>("");
+
+    const onClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+       setSearch(e.target.value)
+       console.log(e)
+    }
   return (
-    <div>Search</div>
+    <div>
+        <form action="">
+            <input type="text" value={search} onChange={(e) => onClick(e)} />
+        </form>
+    </div>
   )
-}
+};
 
 export default Search
