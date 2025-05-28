1d27bf38efbde83b65578f37564cee19e2402fd0
Rorisang19 <rorisangputu@gmail.com>
2025-05-28 11:51:20 +0200

    feat: refactor Card component layout for improved styling and structure

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -10,18 +10,16 @@ interface Props {
 
 const Card: React.FC<Props> = ({id, searchResult, onPortfolioCreate}: Props): JSX.Element => {
   return (
-    <div key={id} id={id} className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg">
-        <img 
-          src="https://static.thenounproject.com/png/2629071-200.png" 
-          alt="company logo" 
-        />
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className="text-center">{searchResult.name}</h1>
-            <h2>({searchResult.symbol})</h2>
-            <p>{searchResult.currency}</p>
-            <h3>{searchResult.exchangeFullName}</h3>
-        </div>
-        <p className='text-center'>
+    <div
+      className="flex flex-col items-center justify-between gap-5 w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-veryDarkViolet md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-veryDarkBlue">{searchResult.currency}</p>
+      <p className="font-bold text-veryDarkBlue">
           {searchResult.stockExchange}
         </p>
         <AddPortfolio 
