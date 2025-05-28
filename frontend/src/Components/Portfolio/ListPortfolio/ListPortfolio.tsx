03d7f99953330cc6ef5dc0a2b26ff7409e809e11
Rorisang19 <rorisangputu@gmail.com>
2025-05-28 11:50:40 +0200

    feat: refactor ListPortfolio component for improved layout and user feedback

--- frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
+++ frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
@@ -8,17 +8,29 @@ interface Props {
 
 const ListPortfolio = ({portfolioValues, onPortfolioDelete}: Props) => {
   return (
-    <div className='flex flex-col p-2 my-2 border-gray-400 border rounded-md'>
-        <h3>My Portfolio</h3>
-        <ul>
-            {portfolioValues &&
-                portfolioValues.map((value) => {
-                    return <CardPortfolio portfolioValue={value} onPortfolioDelete={onPortfolioDelete}/>
-                })
-            }
-
-        </ul>
-    </div>
+    <section id="portfolio">
+      <h2 className="mb-3 mt-3 text-3xl font-semibold text-center md:text-4xl">
+        My Portfolio
+      </h2>
+      <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 mb-5 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
+        <>
+          {portfolioValues.length > 0 ? (
+            portfolioValues.map((portfolioValue) => {
+              return (
+                <CardPortfolio
+                  portfolioValue={portfolioValue}
+                  onPortfolioDelete={onPortfolioDelete}
+                />
+              );
+            })
+          ) : (
+            <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+              Your portfolio is empty.
+            </h3>
+          )}
+        </>
+      </div>
+    </section>
   )
 }
 
