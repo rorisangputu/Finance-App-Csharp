d7ee5650488742c735c206125ac7c2909005efeb
Rorisang19 <rorisangputu@gmail.com>
2025-05-28 11:51:00 +0200

    feat: enhance AddPortfolio component layout for improved styling and structure

--- frontend/src/Components/Portfolio/AddPortfolio/AddPortfolio.tsx
+++ frontend/src/Components/Portfolio/AddPortfolio/AddPortfolio.tsx
@@ -7,10 +7,17 @@ interface Props {
 
 function AddPortfolio({onPortfolioCreate, symbol}: Props) {
   return (
-    <form onSubmit={onPortfolioCreate}>
-        <input readOnly={true} hidden={true} value={symbol}/>
-        <button type='submit' className='py-1 px-3 bg-gray-500 text-white'>Add To Portfolio</button>
-    </form>
+    <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+      <form onSubmit={onPortfolioCreate}>
+        <input readOnly={true} hidden={true} value={symbol} />
+        <button
+          type="submit"
+          className="p-2 px-8 text-white bg-blue-950 rounded-lg hover:opacity-70 focus:outline-none"
+        >
+          Add
+        </button>
+      </form>
+    </div>
   )
 }
 
