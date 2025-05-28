366e44ed7c2bbc6fa8cf062df7a276a76813c8ea
Rorisang19 <rorisangputu@gmail.com>
2025-05-28 10:55:17 +0200

    feat: pass onPortfolioDelete prop to CardPortfolio in ListPortfolio component

--- frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
+++ frontend/src/Components/Portfolio/ListPortfolio/ListPortfolio.tsx
@@ -1,17 +1,19 @@
+import type { SyntheticEvent } from 'react';
 import CardPortfolio from '../CardPortfolio/CardPortfolio';
 
 interface Props {
     portfolioValues: string[];
+    onPortfolioDelete: (e: SyntheticEvent) => void;
 }
 
-const ListPortfolio = ({portfolioValues}: Props) => {
+const ListPortfolio = ({portfolioValues, onPortfolioDelete}: Props) => {
   return (
     <div className='flex flex-col p-2 my-2 border-gray-400 border rounded-md'>
         <h3>My Portfolio</h3>
         <ul>
             {portfolioValues &&
                 portfolioValues.map((value) => {
-                    return <CardPortfolio portfolioValue={value}/>
+                    return <CardPortfolio portfolioValue={value} onPortfolioDelete={onPortfolioDelete}/>
                 })
             }
 
