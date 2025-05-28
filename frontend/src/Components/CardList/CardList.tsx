7f9c7cc66b6ef10bbf5a84c9bac407c5c52568ec
Rorisang19 <rorisangputu@gmail.com>
2025-05-28 11:51:15 +0200

    feat: update no results message in CardList component for improved styling

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -23,7 +23,9 @@ const CardList = ({searchResults, onPortfolioCreate}:Props) => {
                 )
             })
         ) : (
-            <h1>No Results</h1>
+            <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+                No results!
+            </p>
         ) }
     </div>
   )
