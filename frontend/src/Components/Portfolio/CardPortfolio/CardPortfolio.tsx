c63af1e236346bd4315688e425c79387a2177446
Rorisang19 <rorisangputu@gmail.com>
2025-05-28 11:50:54 +0200

    feat: refactor CardPortfolio layout for improved styling and structure

--- frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 const CardPortfolio = ({portfolioValue, onPortfolioDelete}: Props) => {
   return (
-    <div className="flex gap-2 justify-between">
-        <h4>{portfolioValue}</h4>
-        <DeletePortfolio portfolioValue={portfolioValue} onPortfolioDelete={onPortfolioDelete}/>
+    <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
+      <p className="pt-6 text-xl font-bold">{portfolioValue}</p>
+      <DeletePortfolio portfolioValue={portfolioValue} onPortfolioDelete={onPortfolioDelete}/>
     </div>
   )
 }
