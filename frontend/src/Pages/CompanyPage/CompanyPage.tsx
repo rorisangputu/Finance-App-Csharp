87857806182a4d255e50a405b67c71304528db12
Rorisang19 <rorisangputu@gmail.com>
2025-06-02 13:50:47 +0200

    feat: refactor CompanyPage component to remove Props interface and enhance data handling

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import { useParams } from "react-router"
+import { getCompanyProfile } from "../../api";
+import { useEffect, useState } from "react";
+import type { CompanyProfile } from "../../company";
 
-interface Props{
 
-}
 
-const CompanyPage = (props: Props) => {
+
+const CompanyPage = () => {
+  const {ticker} = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const res = await getCompanyProfile(ticker as string);
+      if (typeof res === "object" && res !== null && "data" in res) {
+        setCompany(res.data[0]);
+      } else {
+        setCompany(undefined);
+      }
+    }
+
+    getProfile();
+  }, [ticker])
+
   return (
-    <div>CompanyPage</div>
+    <div>
+      {
+        company ? 
+        (
+          <>
+            <div>{company?.companyName}</div>
+          </>
+        ) : (
+          <>
+            <div className="border border-red-600 bg-red-600 p-5
+              flex flex-col justify-center items-center text-white gap-5"
+            >
+              <h1 className="text-5xl font-bold">No Data.</h1>
+              <p className="font-medium">Company not found.</p>
+            </div>
+          </>
+        )
+      }
+      
+    </div>
   )
 }
 
