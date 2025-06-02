6e6ea1bc6fbfbe7812d3ffdc454c332dc27b37b4
Rorisang19 <rorisangputu@gmail.com>
2025-06-02 12:04:07 +0200

    feat: refactor api.tsx to define apiKey once and add getCompanyProfile function

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { type CompanySearch } from "./company"
+import { type CompanyProfile, type CompanySearch } from "./company"
 interface searchRes{
     data: CompanySearch[];
 }
+const apiKey = import.meta.env.VITE_API_KEY;
+
 export const searchCompanies = async(query: string) => {
-    const apiKey = import.meta.env.VITE_API_KEY
+    
     try {
         const data = await axios.get<searchRes>(
             `https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${apiKey}`
@@ -13,5 +15,15 @@ export const searchCompanies = async(query: string) => {
     } catch (error) {
         console.log(error)
         return "An unexpected error has occurred"
+    }
+}
+
+export const getCompanyProfile = async(query: string) => {
+    try {
+        const data = await axios.get<CompanyProfile[]>(
+            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${apiKey}`
+        )
+    } catch (error) {
+        
     }
 }
