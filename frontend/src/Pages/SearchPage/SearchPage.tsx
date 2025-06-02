a0c1550f04e80b63f036d89c0fa361098ae76182
Rorisang19 <rorisangputu@gmail.com>
2025-06-02 11:23:44 +0200

    feat: implement SearchPage with search functionality and portfolio management

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState, type SyntheticEvent } from 'react'
+import Search from '../../Components/Search/Search'
+import ListPortfolio from '../../Components/Portfolio/ListPortfolio/ListPortfolio'
+import CardList from '../../Components/CardList/CardList'
+import { searchCompanies } from '../../api'
+import type { CompanySearch } from '../../company'
 
 interface Props  {}
 
 const SearchPage = (props: Props) => {
+
+  const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([])
+  const [searchResult, setSearchRes] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>('');
+
+  const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearch(e.target.value)
+    //console.log(e, search)
+  }
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+
+    //Type checking
+    if(typeof result === 'string'){
+      setServerError(result);
+    }else if(Array.isArray(result.data)){
+      setSearchRes(result.data)
+    }
+
+    console.log(searchResult);
+  }
+
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value)
+    if(exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value]
+    setPortfolioValues(updatedPortfolio)
+  }
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return  value !== e.target[0].value;
+    });
+    setPortfolioValues(removed)
+  }
+
   return (
-    <div>SearchPage</div>
+    <main>
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
+      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
+      {serverError && <h1>{serverError}</h1>}
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
+    </main>
   )
 }
 
