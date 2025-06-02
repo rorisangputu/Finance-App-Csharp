a1d92c74bef389b684aa15c5a92330efd4edbd27
Rorisang19 <rorisangputu@gmail.com>
2025-06-02 11:23:21 +0200

    refactor: simplify App component by removing unused state and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,66 +1,15 @@
-import { useState, type SyntheticEvent } from "react";
-import "./App.css";
-import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Search/Search";
-import { type CompanySearch } from "./company";
-import { searchCompanies } from "./api";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import { Outlet } from "react-router";
 import Navbar from "./Components/Navbar/Navbar";
 
 
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([])
-  const [searchResult, setSearchRes] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>('');
-
-  const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setSearch(e.target.value)
-    //console.log(e, search)
-  }
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-
-    //Type checking
-    if(typeof result === 'string'){
-      setServerError(result);
-    }else if(Array.isArray(result.data)){
-      setSearchRes(result.data)
-    }
-
-    console.log(searchResult);
-  }
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find((value) => value === e.target[0].value)
-    if(exists) return;
-    const updatedPortfolio = [...portfolioValues, e.target[0].value]
-    setPortfolioValues(updatedPortfolio)
-  }
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return  value !== e.target[0].value;
-    });
-    setPortfolioValues(removed)
-  }
+  
 
   return (
     <>
-      
-        <Navbar/>
-        <main>
-          <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
-          <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
-          {serverError && <h1>{serverError}</h1>}
-          <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
-        </main>
-      
+      <Navbar/>
+      <Outlet/>
     </>
   );
 }
