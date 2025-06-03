ae214a13dc732272bc685b54d5f3031463396fbd
Rorisang19 <rorisangputu@gmail.com>
2025-06-03 15:07:01 +0200

    feat: add route for CashFlowStatement component in router configuration

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -7,21 +7,16 @@ import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
+import CashFlowStatement from "../Components/CashFlowStatement/CashFlowStatement";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>,
         children: [
-            {
-                path: '', element: <HomePage/>
-            },
-            {
-                path: 'search', element: <SearchPage/>
-            },
-            {
-                path: 'design-guide', element: <DesignGuide/>
-            },
+            { path: '', element: <HomePage/> },
+            { path: 'search', element: <SearchPage/> },
+            { path: 'design-guide', element: <DesignGuide/> },
             {
                 path: 'company/:ticker', 
                 element: <CompanyPage/>,
@@ -29,6 +24,7 @@ export const router = createBrowserRouter([
                     { path: "company-profile", element: <CompanyProfile/> },
                     { path: "income-statement", element: <IncomeStatement/> },
                     { path: "balance-sheet", element: <BalanceSheet/> },
+                    { path: "cashflow-statement", element: <CashFlowStatement/> },
                 ]
             },
         
