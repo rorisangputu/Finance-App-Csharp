e32576f9cb5e9540dd254b642b71a199d6bba593
Rorisang19 <rorisangputu@gmail.com>
2025-06-03 09:39:53 +0200

    feat: create DesignPage component with header and Table integration

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+        <h1>FinShark Design Page</h1>
+        <h2>
+            This is FinShark's design page. This is where we will
+            house various design aspects of the app
+        </h2>
+        <Table/>
+    </>
+  )
+}
+
+export default DesignPage
