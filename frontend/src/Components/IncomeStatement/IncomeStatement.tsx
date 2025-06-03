49a3754a0a8c616fdb73841df901307412508871
Rorisang19 <rorisangputu@gmail.com>
2025-06-03 13:35:00 +0200

    feat: enhance IncomeStatement component with detailed financial metrics configuration

--- frontend/src/Components/IncomeStatement/IncomeStatement.tsx
+++ frontend/src/Components/IncomeStatement/IncomeStatement.tsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import { formatLargeMonetaryNumber, formatRatio } from '../../Helpers/NumberFormatting';
+import type { CompanyIncomeStatement } from '../../company';
 
-type Props = {}
+const configs = [
+  {
+    label: "Date",
+    render: (company: CompanyIncomeStatement) => company.date,
+  },
+  {
+    label: "Revenue",
+    render: (company: CompanyIncomeStatement) =>
+      formatLargeMonetaryNumber(company.revenue),
+  },
+  {
+    label: "Cost Of Revenue",
+    render: (company: CompanyIncomeStatement) =>
+      formatLargeMonetaryNumber(company.costOfRevenue),
+  },
+  {
+    label: "Depreciation",
+    render: (company: CompanyIncomeStatement) =>
+      formatLargeMonetaryNumber(company.depreciationAndAmortization),
+  },
+  {
+    label: "Operating Income",
+    render: (company: CompanyIncomeStatement) =>
+      formatLargeMonetaryNumber(company.operatingIncome),
+  },
+  {
+    label: "Income Before Taxes",
+    render: (company: CompanyIncomeStatement) =>
+      formatLargeMonetaryNumber(company.incomeBeforeTax),
+  },
+  {
+    label: "Net Income",
+    render: (company: CompanyIncomeStatement) =>
+      formatLargeMonetaryNumber(company.netIncome),
+  },
+  {
+    label: "Net Income Ratio",
+    render: (company: CompanyIncomeStatement) =>
+      formatRatio(company.netIncomeRatio),
+  },
+  {
+    label: "Earnings Per Share",
+    render: (company: CompanyIncomeStatement) => formatRatio(company.eps),
+  },
+  {
+    label: "Earnings Per Diluted",
+    render: (company: CompanyIncomeStatement) =>
+      formatRatio(company.epsdiluted),
+  },
+  {
+    label: "Gross Profit Ratio",
+    render: (company: CompanyIncomeStatement) =>
+      formatRatio(company.grossProfitRatio),
+  },
+  {
+    label: "Opearting Income Ratio",
+    render: (company: CompanyIncomeStatement) =>
+      formatRatio(company.operatingIncomeRatio),
+  },
+  {
+    label: "Income Before Taxes Ratio",
+    render: (company: CompanyIncomeStatement) =>
+      formatRatio(company.incomeBeforeTaxRatio),
+  },
+];
 
-const IncomeStatement = (props: Props) => {
+const IncomeStatement = () => {
   return (
     <div>IncomeStatement</div>
   )
