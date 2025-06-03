bb56603b5e855da507551b45c57e54937b01c8a1
Rorisang19 <rorisangputu@gmail.com>
2025-06-03 11:48:51 +0200

    feat: add DesignGuide component with table configuration and layout

--- frontend/src/Pages/DesignGuide/DesignGuide.tsx
+++ frontend/src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,8 +1,19 @@
 import Table from '../../Components/Table/Table'
 import RatioList from '../../Components/RatioList/RatioList'
+import { formatLargeNonMonetaryNumber } from '../../Helpers/NumberFormatting'
+import type { CompanyKeyMetrics } from '../../company'
+import { testIncomeStatementData } from '../../Components/Table/testData';
 
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: CompanyKeyMetrics) =>
+      formatLargeNonMonetaryNumber(company.marketCapTTM),
+    subTitle: "Total value of all a company's shares of stock",
+  },
+];
 
-const DesignPage = () => {
+const DesignGuide = () => {
   return (
     <>
         <h1 className='font-medium text-xl'>FinShark Design Page</h1>
@@ -10,7 +21,7 @@ const DesignPage = () => {
             This is FinShark's design page. This is where we will
             house various design aspects of the app.
         </h2>
-        <RatioList/>
+        <RatioList data={testIncomeStatementData} config={tableConfig}/>
         <Table/>
         <h3>
             Table - Table takes in a configuration object and company data as params.
@@ -20,4 +31,4 @@ const DesignPage = () => {
   )
 }
 
-export default DesignPage
+export default DesignGuide
