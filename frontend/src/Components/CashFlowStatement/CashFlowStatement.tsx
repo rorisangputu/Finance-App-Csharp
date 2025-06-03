8a7768bf8072b850624858e8dfb84a72b8bf2420
Rorisang19 <rorisangputu@gmail.com>
2025-06-03 15:23:58 +0200

    feat: implement CashFlowStatement component with data fetching and display

--- frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
+++ frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
@@ -1,9 +1,70 @@
+import { useOutletContext } from "react-router";
+import type { CompanyCashFlow } from "../../company";
+import { formatLargeMonetaryNumber } from "../../Helpers/NumberFormatting";
+import { useEffect, useState } from "react";
+import Table from "../Table/Table";
+import Spinner from "../Spinners/Spinner";
+import { getCashFlow } from "../../api";
 
+const config = [
+  {
+    label: "Date",
+    render: (company: CompanyCashFlow) => company.date,
+  },
+  {
+    label: "Operating Cashflow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.operatingCashFlow),
+  },
+  {
+    label: "Investing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.netCashUsedForInvestingActivites),
+  },
+  {
+    label: "Financing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(
+        company.netCashUsedProvidedByFinancingActivities
+      ),
+  },
+  {
+    label: "Cash At End of Period",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.cashAtEndOfPeriod),
+  },
+  {
+    label: "CapEX",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.capitalExpenditure),
+  },
+  {
+    label: "Issuance Of Stock",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.commonStockIssued),
+  },
+  {
+    label: "Free Cash Flow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.freeCashFlow),
+  },
+];
 
 const CashFlowStatement = () => {
-  return (
-    <div>CashFlowStatement</div>
-  )
-}
+    const ticker = useOutletContext<string>();
+    const [cashFlowData, setCashFlowData] = useState<CompanyCashFlow[]>();
+    useEffect(() => {
+    const getRatios = async () => {
+      const result = await getCashFlow(ticker);
+      setCashFlowData(result!.data);
+    };
+    getRatios();
+  }, [ticker]);
+  return cashFlowData ? (
+    <Table configs={config} data={cashFlowData}></Table>
+  ) : (
+    <Spinner/>
+  );
+};
 
 export default CashFlowStatement
