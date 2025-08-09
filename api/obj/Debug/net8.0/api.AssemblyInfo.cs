3b0902a7d8e98322f19aa2e8350e5ed0750497d3
Rorisang Putu <rorisangputu@gmail.com>
2025-08-09 21:13:01 +0200

    database creation

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa352abaa5bc21569e1cff25a0621c658a9b1309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad624242b1110182909c84659c00de0ade1f66b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
