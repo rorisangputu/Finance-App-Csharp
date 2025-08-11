fa3f3ac0d126c40a0c2553813ff3d0942fe56127
Rorisang Putu <rorisangputu@gmail.com>
2025-08-11 22:58:23 +0200

    adding delete comment functionality

--- api/Repository/CommentRespository.cs
+++ api/Repository/CommentRespository.cs
@@ -24,6 +24,18 @@ namespace api.Repository
             return commentModel;
         }
 
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(c => c.Id == id);
+            if (commentModel == null)
+            {
+                return null;
+            }
+            _context.Comments.Remove(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comments.ToListAsync();
@@ -34,6 +46,9 @@ namespace api.Repository
             return await _context.Comments.FindAsync(id);
         }
 
-
+        public Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
