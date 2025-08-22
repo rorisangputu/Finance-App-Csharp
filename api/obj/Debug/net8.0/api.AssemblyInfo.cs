f321f0f02c0c41e306a507d06926130d28718e41
Rorisang Putu <rorisangputu@gmail.com>
2025-08-22 21:57:21 +0200

    adding account files

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5494167ee096652281b63a07edecc8a013b578dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f6b4a59e149a4ee129462e31bff5abaff4ecc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
