8e6355eab2391d2e5fdd4648dad31d41b58bb13f
Rorisang Putu <rorisangputu@gmail.com>
2025-08-28 16:30:54 +0200

    new account controller

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Account;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -19,9 +20,41 @@ namespace api.Controllers
             _userManager = userManager;
         }
 
+        [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
-            
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+
+                var appUser = new AppUser
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email,
+                };
+                var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User created");
+                    }
+                    else
+                    {
+                        return StatusCode(400, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(400, createdUser.Errors);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
         }
     }
 }
