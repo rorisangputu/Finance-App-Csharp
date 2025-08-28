da75b1ec0d2e43fa74e0267830a189cf0ca659a4
Rorisang Putu <rorisangputu@gmail.com>
2025-08-28 19:36:16 +0200

    commit

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c83d0d11644270aa7ff28417da8afd07c23ab50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22caf54eb7f1850c58a65e310fda471ad689ffb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
