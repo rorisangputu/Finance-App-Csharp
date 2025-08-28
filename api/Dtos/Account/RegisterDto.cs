08570f79d8e838a9406a6237eed1ba3d2761c9f4
Rorisang Putu <rorisangputu@gmail.com>
2025-08-28 16:31:11 +0200

    registerdto

--- api/Dtos/Account/RegisterDto.cs
+++ api/Dtos/Account/RegisterDto.cs
@@ -13,5 +13,7 @@ namespace api.Dtos.Account
         [Required]
         [EmailAddress]
         public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
     }
 }
