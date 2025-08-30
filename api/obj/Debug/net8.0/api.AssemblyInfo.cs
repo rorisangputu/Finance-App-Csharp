da075ff4217538f7dd7928e9b3694e406fe1b7d6
Rorisang Putu <rorisangputu@gmail.com>
2025-08-31 01:21:42 +0200

    run

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838319345b2907aa177925d26e57accbbc044e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e34434ce74a9b289b49724f9c7280bfb39365cda")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
