b415b539ff8a78c27a7ee4c330f56f9eb237d574
Rorisang Putu <rorisangputu@gmail.com>
2025-08-30 13:55:35 +0200

    adding portfiolio repo to app scope

--- api/Program.cs
+++ api/Program.cs
@@ -89,6 +89,7 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRespository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
 
 var app = builder.Build();
 
