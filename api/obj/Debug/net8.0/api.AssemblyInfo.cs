e3fb90c81174a66dcd74d544f600ee43a86de8a2
Rorisang Putu <rorisangputu@gmail.com>
2025-09-11 11:32:55 +0200

    Login Page

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aaf9bf70416344967e89bf81033e2674a5ada12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3aaf190811a1d3a9c41b8f84f6f5e766197466")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -8,27 +8,27 @@ import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 import CashFlowStatement from "../Components/CashFlowStatement/CashFlowStatement";
+import LoginPage from "../Pages/LoginPage/LoginPage";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App/>,
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "search", element: <SearchPage /> },
+      { path: "design-guide", element: <DesignGuide /> },
+      {
+        path: "company/:ticker",
+        element: <CompanyPage />,
         children: [
-            { path: '', element: <HomePage/> },
-            { path: 'search', element: <SearchPage/> },
-            { path: 'design-guide', element: <DesignGuide/> },
-            {
-                path: 'company/:ticker', 
-                element: <CompanyPage/>,
-                children: [
-                    { path: "company-profile", element: <CompanyProfile/> },
-                    { path: "income-statement", element: <IncomeStatement/> },
-                    { path: "balance-sheet", element: <BalanceSheet/> },
-                    { path: "cashflow-statement", element: <CashFlowStatement/> },
-                ]
-            },
-        
-        ]
-    }
-])
-
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashFlowStatement /> },
+        ],
+      },
+    ],
+  },
+]);
